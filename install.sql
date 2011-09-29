0456a2da4f915fa7aaa153757c653158a47e5f7e
Oliver Kliebisch <oliver@kliebisch.net>
2011-09-30 00:09:01 +0200

    -changed defaults

--- install.sql
+++ install.sql
@@ -1,1 +1,1 @@
-ALTER TABLE wbb1_1_post ADD markAsTeamMessage tinyint(1) NOT NULL default 0;
+ALTER TABLE wbb1_1_post ADD markAsTeamMessage tinyint(1) NOT NULL default 1;
---
+++ update.sql
@@ -0,0 +1,1 @@
+ALTER TABLE  wbb1_1_post CHANGE  markAsTeamMessage  markAsTeamMessage TINYINT( 1 ) NOT NULL DEFAULT  '1'
